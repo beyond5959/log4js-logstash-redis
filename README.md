c0cbb87476b0fb054eab0846e1572650a24f48ee
beyond5959 <liuxin5959@gmail.com>
2016-08-23 16:16:41 +0800

    first commit.

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # log4js-logstash-redis
 A simple log appender for log4js that sends the data to logstash by redis.
+
+
+Installation
+------------
+```
+npm install log4js-logstash-redis --save
+```
+
+Usage: logstash configuration
+-----------------------------
+In the "input" part of the logstash server conf :
+```
+input {
+  redis {
+  	codec => json
+  	data_type => "list"
+  	key => "YOURKEY"
+  }
+}
+```
+
+Usage: log4js configuration
+---------------------------
+Plain javascript
+```javascript
+  const log4js = require('log4js');
+  log4js.configure({
+      "appenders": [{
+          "type": "log4js-logstash-redis",
+          "key": "YOURKEY",
+          "redis": {
+              "host": "locahost",
+              "port": 6379,
+              "db": 0 // default 0
+          }
+      }]
+  });
+
+  const logger = log4js.getLogger('tests');
+
+  logger.debug('hello hello');
+```
---
+++ index.js
@@ -0,0 +1,52 @@
+"use strict";
+const Redis = require('ioredis');
+const util = require('util');
+const layouts = require('log4js').layouts;
+
+function logstashRedis(config, layout) {
+  let redis = config.redis ? new Redis(config.redis) : new Redis();
+  let type = config.logType ? config.logType : config.category;
+  layout = layout || layouts.dummyLayout;
+  if(!config.fields) {
+    config.fields = {};
+  }
+
+  return function log(loggingEvent) {
+    if (loggingEvent.data.length > 1) {
+      let secondEvData = loggingEvent.data[1];
+      for (let k in secondEvData) {
+        config.fields[k] = secondEvData[k];
+      }
+    }
+    config.fields.level = loggingEvent.level.levelStr;
+
+    let logObject = {
+      "@version" : "1",
+      "@timestamp" : (new Date(loggingEvent.startTime)).toISOString(),
+      "type" : config.logType ? config.logType : config.category,
+      "message" : layout(loggingEvent),
+      "fields" : JSON.stringify(config.fields)
+    };
+    sendLog(redis, config.key, logObject);
+  };
+}
+
+function sendLog(redis, key, logObject) {
+  let logString = JSON.stringify(logObject);
+  redis.rpush(key, logString, function (err, result) {
+    if (err) {
+      console.error("log4js-logstash-redis - Error: %s", util.inspect(err))
+    }
+  });
+}
+
+function configure(config) {
+  let layout;
+  if (config.layout) {
+    layout = layouts.layout(config.layout.type, config.layout);
+  }
+  return logstashRedis(config, layout);
+}
+
+exports.appender = logstashRedis;
+exports.configure = configure;
